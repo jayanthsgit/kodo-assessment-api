f3791d5df8bb6370955b831dfb5fed685cd377ae
Jayanth <jayanthknimmalapalli@gmail.com>
2021-09-13 12:33:27 +0530

    cors fix

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const router = require("./controller/routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', router);
 
 app.listen(process.env.PORT || 8081, ()=>{console.log(`app listening on port ${process.env.PORT || 8081}`)});
